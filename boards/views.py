d47ea96aa3c9b15c11ed0cd1f750107d6fbf8e71
Ritter Gustave <Gustaver@uci.edu>
2018-04-26 14:01:45 -0700

    Fixed password routing, fixed post edit hack, added CBV's

--- boards/views.py
+++ boards/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView, ListView
 from django.contrib.auth.models import User
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
 from django.db.models import Count
+from django.utils import timezone
 from django.http import Http404
 # create your views here.
 
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards': boards})
+# def home(request):
+#     boards = Board.objects.all()
+#     return render(request, 'home.html', {'boards': boards})
 
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk = pk)
@@ -62,3 +69,23 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+@method_decorator(login_required, name = 'dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message',)
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by = self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit = False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk = post.topic.board.pk, topic_pk = post.topic.pk)
